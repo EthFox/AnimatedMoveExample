d183c7ca9a1d4ccc422e8cd8b0ae05f337ada36f
EthFox <168486262+EthFox@users.noreply.github.com>
2024-04-30 18:14:38 -0400

    Add project files.

---
+++ frmMoveAnimationExample.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AnimatedMoveExample
+{
+	partial class frmMoveAnimationExample
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.animatedSquare = new System.Windows.Forms.PictureBox();
+			this.MoveStaggerTimer = new System.Windows.Forms.Timer(this.components);
+			this.unanimatedSquare = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.animatedSquare)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.unanimatedSquare)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// animatedSquare
+			// 
+			this.animatedSquare.BackColor = System.Drawing.Color.DodgerBlue;
+			this.animatedSquare.Location = new System.Drawing.Point(171, 193);
+			this.animatedSquare.Name = "animatedSquare";
+			this.animatedSquare.Size = new System.Drawing.Size(50, 50);
+			this.animatedSquare.TabIndex = 1;
+			this.animatedSquare.TabStop = false;
+			// 
+			// MoveStaggerTimer
+			// 
+			this.MoveStaggerTimer.Interval = 10;
+			this.MoveStaggerTimer.Tick += new System.EventHandler(this.MoveStaggerTimer_Tick);
+			// 
+			// unanimatedSquare
+			// 
+			this.unanimatedSquare.BackColor = System.Drawing.Color.DodgerBlue;
+			this.unanimatedSquare.Location = new System.Drawing.Point(530, 193);
+			this.unanimatedSquare.Name = "unanimatedSquare";
+			this.unanimatedSquare.Size = new System.Drawing.Size(50, 50);
+			this.unanimatedSquare.TabIndex = 2;
+			this.unanimatedSquare.TabStop = false;
+			// 
+			// frmMoveAnimationExample
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.unanimatedSquare);
+			this.Controls.Add(this.animatedSquare);
+			this.Name = "frmMoveAnimationExample";
+			this.Text = "Move \"Animation\" Example";
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmMoveAnimationExample_KeyPress);
+			((System.ComponentModel.ISupportInitialize)(this.animatedSquare)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.unanimatedSquare)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox animatedSquare;
+		private System.Windows.Forms.Timer MoveStaggerTimer;
+		private System.Windows.Forms.PictureBox unanimatedSquare;
+	}
+}
+
---
+++ frmMoveAnimationExample.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimatedMoveExample
+{
+	public partial class frmMoveAnimationExample : Form
+	{
+		public frmMoveAnimationExample()
+		{
+			InitializeComponent();
+		}
+
+		// allow for easy changes of how smooth you want the move to be (staggerNumber) and how far it goes (staggerAmount) \\
+		const int staggerNumber = 5;
+		const int moveAmount = 50;
+
+		Point OffsetAmount; // allows calculations in tick event to work no matter the direction
+		int staggerCounter; // stop the timer once the move has finished
+
+		private void frmMoveAnimationExample_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// Modification of KeyPress example \\
+			char KeyPressed = e.KeyChar; 
+			
+			switch (KeyPressed)
+			{
+				case 'w':
+					OffsetAmount = new Point(0, -moveAmount);
+					break;
+				case 'a':
+					OffsetAmount = new Point(-moveAmount, 0);
+					break;
+				case 's':
+					OffsetAmount = new Point(0, moveAmount);
+					break;
+				case 'd':
+					OffsetAmount = new Point(moveAmount, 0);
+					break;
+				case '\u001b':
+					this.Close();
+					break;
+			}
+
+			MoveStaggerTimer.Start(); // once the direction to move has been found, start the move process
+
+			// move unanimated square for comparison
+			Point unanimatedLoc = unanimatedSquare.Location;
+			unanimatedLoc.Offset(OffsetAmount);
+			unanimatedSquare.Location = unanimatedLoc;
+		}
+
+		private void MoveStaggerTimer_Tick(object sender, EventArgs e) 
+		{ // interval property currently set to 10, 100 times a second
+			Point squareLoc = animatedSquare.Location;
+
+			// stagger by the correct portion of the whole move distance
+			squareLoc.Offset(OffsetAmount.X / staggerNumber, OffsetAmount.Y / staggerNumber); 
+			++staggerCounter; // increment counter
+
+			animatedSquare.Location = squareLoc; // move to offset location
+
+			if (staggerCounter == staggerNumber) // if move is finished
+			{
+				MoveStaggerTimer.Stop(); // stop timer
+				staggerCounter = 0; // reset counter
+			}
+		}
+	}
+}
